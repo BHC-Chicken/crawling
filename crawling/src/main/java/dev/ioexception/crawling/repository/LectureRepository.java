f5166f9bbad58396ad39fe70cc346f3958a9730b
BHC-Chicken <tij05078@gmail.com>
2024-12-25 01:05:03 +0900

    repository 쿼리 재작성

--- crawling/src/main/java/dev/ioexception/crawling/repository/LectureRepository.java
+++ crawling/src/main/java/dev/ioexception/crawling/repository/LectureRepository.java
@@ -1,18 +1,27 @@
 package dev.ioexception.crawling.repository;
 
+import dev.ioexception.crawling.dto.response.LectureYearPriceDBResponse;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 import dev.ioexception.crawling.entity.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
-	Optional<Lecture> findLectureByLectureId(String lectureId);
-	Optional<Lecture> findByLectureIdAndDate(String lectureId, LocalDate now);
-	List<Lecture> findAllByTitleContainingAndDate(String title, LocalDate localDate);
+    Optional<Lecture> findLectureByLectureId(String lectureId);
+    Optional<Lecture> findByLectureIdAndDate(String lectureId, LocalDate now);
+    List<Lecture> findAllByTitleContainingAndDate(String title, LocalDate localDate);
+    List<Lecture> findAllByDate(LocalDate localDate);
 
-	List<Lecture> findAllByDate(LocalDate localDate);
+    @Query(value = "SELECT DATE_FORMAT(date, '%Y-%m') AS month, MIN(sale_Price) AS min_price " +
+            "FROM lecture " +
+            "WHERE lecture_Id = :lectureId AND " +
+            "date >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) " +
+            "GROUP BY DATE_FORMAT(date, '%Y-%m')", nativeQuery = true)
+    List<Object[]> findMonthlyMinPrices(@Param("lectureId") String lectureId);
 }
--- crawling/src/main/java/dev/ioexception/crawling/repository/LectureTagRepository.java
+++ crawling/src/main/java/dev/ioexception/crawling/repository/LectureTagRepository.java
@@ -2,8 +2,11 @@ package dev.ioexception.crawling.repository;
 
 import dev.ioexception.crawling.entity.LectureTag;
 import dev.ioexception.crawling.repository.query.LectureTagRepositoryCustom;
+
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,11 +15,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LectureTagRepository extends JpaRepository<LectureTag, Long>, LectureTagRepositoryCustom {
-    //    List<LectureTag> findAllByLecture_LectureId(String lectureId);
     @EntityGraph(attributePaths = {"tag", "lecture"})
     @Query("SELECT lt FROM LectureTag lt WHERE lt.lecture.lectureId = :lectureId AND lt.lecture.date = :localDate")
     List<LectureTag> getLectureTagsWithTagByLectureId(@Param("lectureId") String lectureId,
                                                       @Param("localDate") LocalDate localDate);
-
 }
-
